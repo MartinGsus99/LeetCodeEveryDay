0f4ca18c44188be00fcb9e81e83311ace3b9877a
Martin Wang <65059824+MartinGsus99@users.noreply.github.com>
2023-03-04 12:04:57 +0800

    Update 114.js

--- JS/114.js
+++ JS/114.js
@@ -10,6 +10,28 @@
  * @param {TreeNode} root
  * @return {void} Do not return anything, modify root in-place instead.
  */
+
+//先先序遍历将所有的结点放到一个list，然后遍历list；
+//94.76  39.01
 var flatten = function(root) {
+    const NodeList=[];
+
+    var traverse=(node)=>{
+        if(!node){
+            return;
+        }
+        NodeList.push(node);
+        traverse(node.left);
+        traverse(node.right);
+    }
+
+    traverse(root);
+
+    for(let i=1;i<NodeList.length;i++){
+        let pre=NodeList[i-1];
+        let cur=NodeList[i];
+        pre.left=null;
+        pre.right=cur;
+    }
 
 };
