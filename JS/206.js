db020b10587f1d6f4aea2f7679b2d903ef1f7895
Martin Wang <65059824+MartinGsus99@users.noreply.github.com>
2023-03-06 21:25:34 +0800

    206

---
+++ JS/206.js
@@ -0,0 +1,105 @@
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+//广度优先算法，从左上角开始，使用hash表记录访问过的土地；
+//只有上下可以加入土地，每次到一个方块将他的上下左右都加入到待访问队列；
+var numIslands = function (grid) {
+
+    var visited = new Set();            //存放已经访问的点
+    var queue = new Array();            //存放当前结点相邻的结点
+    //推入0，0
+
+    var count = 0;                 //记录岛屿数量
+    if (grid.length == 0) {
+        return count;
+    }
+
+    var width = grid.length;            //设置数组x索引上限
+    var height = grid[0].length;        //设置数组y索引上限
+
+    var BFS = function (startNode) {
+
+        queue.push(startNode);
+
+        while (queue.length != 0) {
+            let size = queue.length;
+            for (let i = 0; i < size; i++) {
+                let cur = queue.shift();
+                console.log('set', visited);
+                if (!visited.has(cur.toString())) {
+                    visited.add(cur.toString());
+                    console.log("当前结点：", cur);
+                    let x = cur[0];
+                    let y = cur[1];
+                    //当前访问结点
+
+                    //检查周围的点,第一检查数组是否越界，第二判断是否为1，第三是否访问过；
+                    if (x + 1 < width && grid[x + 1][y] == '1') {
+                        if (!visited.has([x + 1, y].toString())) {
+                            queue.push([x + 1, y])
+                            console.log("压入结点1", [x + 1, y]);
+                        }
+                    }
+                    if (y + 1 < height && grid[x][y + 1] == '1') {
+                        if (!visited.has([x, y + 1].toString())) {
+                            queue.push([x, y + 1])
+                            console.log("压入结点2", [x, y + 1]);
+                        }
+                    }
+                    if (x - 1 >= 0 && grid[x - 1][y] == '1') {
+                        if (!visited.has([x - 1, y].toString())) {
+                            console.log("访问结点：", [x - 1, y]);
+                            queue.push([x - 1, y])
+                            console.log("压入结点3", [x - 1, y]);
+                        }
+                    }
+                    if (y -1 >= 0 && grid[x][y - 1] == '1') {
+                        if (!visited.has([x, y - 1].toString())) {
+                            queue.push([x, y - 1])
+                            console.log("压入结点4", [x, y - 1]);
+                        }
+                    }
+                    console.log("当前队列：", queue);
+                }
+            }
+        }
+        count += 1;
+        console.log("Count:", count);
+    }
+
+    //起始点检索条件：1.必须为1 2.起始点没有被访问过
+    for (let i = 0; i < width; i++) {
+        for (let j = 0; j < height; j++) {
+            if (!visited.has([i, j].toString()) && grid[i][j] == '1') {
+                console.log("开始寻找新的岛屿~");
+                BFS([i, j]);
+            }
+        }
+    }
+
+    return count;
+};
+
+const grid = [
+    ["1", "1", "1", "1", "0"],
+    ["1", "1", "0", "1", "0"],
+    ["1", "1", "0", "0", "0"],
+    ["0", "0", "0", "0", "0"]
+]
+
+const grid2 = [
+    ["1", "1", "0", "0", "0"],
+    ["1", "1", "0", "0", "0"],
+    ["0", "0", "1", "0", "0"],
+    ["0", "0", "0", "1", "1"]
+]
+
+const grid3 = [
+    ["1", "0", "1", "1", "1"],
+    ["1", "0", "1", "0", "1"],
+    ["1", "1", "1", "0", "1"]]
+
+
+
+console.log(numIslands(grid2));
---
+++ Python/temDay.py
@@ -0,0 +1,49 @@
+import requests
+import json
+
+from datetime import datetime
+
+import datetime
+def get_time():
+    year=datetime.datetime.now().year
+    month=datetime.datetime.now().month
+    day=datetime.datetime.now().day
+    current_time = datetime.datetime.now().strftime("%H:%M:%S")
+    print("当前时间:",year,'-',month,'-',day,' ', current_time)
+    return str(year)+'-'+str(month)+'-'+str(day)+' '+str(current_time)
+
+def get_weather():
+        key="6ed6ff84f2731efa31ea59d101db12ca"
+        response = requests.get(f"https://apis.tianapi.com/tianqishiju/index?key="+key)
+        data = json.loads(response.text)
+        print(data)
+        return data['result']['content']
+
+def send_message_TestVX(_item, _message, userid = 'o4Ym86Uq64JsdMG743D2ivcXTI8E'): # 默认发送给自己
+    appid = 'wxa5a859907526f804'
+    secret = 'fc6178abf05b52a3ecb8972b353bac98'
+    template_id = 'FcOLzfSnrq_8DR9fp1dpUFYvc4tAr-mz_ta-boa2Dic'
+    time=get_time()
+    title="Enjoy a nice day~"
+    content = ""
+    # 获取天气数据
+    weather_data=get_weather()
+
+
+    # 获取token
+    response = requests.get(
+        f"https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=" + appid + "&secret=" + secret)
+    data = json.loads(response.text)
+    access_token = data['access_token']
+
+    payload = {'touser': userid, 'template_id': template_id,
+               'data': {'time':{'value':time},'title': {'value': title}, 'content': {'value': content},'weather':{'value':weather_data}}}
+
+    headers = {'header': "Content-Type: application/json;charset=utf-8"}
+    url = 'https://api.weixin.qq.com/cgi-bin/message/template/send?access_token=' + access_token
+
+    json_template = json.dumps(payload)
+    response_send = requests.post(url, headers=headers, data=json_template)
+    print("测试公众号：", response_send.text)
+
+send_message_TestVX("","")
